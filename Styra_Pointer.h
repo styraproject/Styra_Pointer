70ca6e30e5c549e1da9b1bcb8f5854db3847493a
Luke Hindman <luke@platypuscreations.net>
2016-05-25 16:02:49 -0600

    GPLv3
    Added GPLv3 License details to codebase

--- Styra_Pointer.h
+++ Styra_Pointer.h
@@ -1,8 +1,22 @@
-/* Author:  Luke Hindman
- * Date: Sun May  1 09:05:59 MDT 2016
- * Description:  StyraPointer interface for standarized mouse pointer
- *   control in various Styra projects.
- */
+/**
+ * Styra_Pointer provides standarized mouse pointer control for Styra prototypes
+ * 
+ * Copyright (C) 2016  Luke Hindman <luke@platypuscreations.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
 
 #ifndef __STYRA_POINTER_H__
 #define __STYRA_POINTER_H__
--- Styra_Pointer_Analog.cpp
+++ Styra_Pointer_Analog.cpp
@@ -1,8 +1,21 @@
-/* Author: Luke Hindman
- * Date: Fri Apr 29 11:02:46 MDT 2016
- * Description:  Analog mouse pointer control implemented using
- * being() -> update() design paradigm.
- */
+/**
+ * Styra_Pointer provides standarized mouse pointer control for Styra prototypes
+ * 
+ * Copyright (C) 2016  Luke Hindman <luke@platypuscreations.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
 
  #include "Styra_Pointer_Analog.h"
 
